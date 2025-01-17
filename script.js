23471eb794690b843af01a972740a21ac814d11d
Azuprosper <128356285+Azuprosper@users.noreply.github.com>
2025-01-17 14:47:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,14 +20,24 @@ function displayBooks(filteredBooks) {
             <h3>${book.title}</h3>
             <p>${book.author}</p>
             <p>${book.description}</p>
-            <a href="${book.link}"target="_blank" >
-            <button onclick="borrowBook(${book.id})" >Read</button>
+            <a href="${book.pdf_link}" target="_blank">
+                <button id="button-book-${book.id}">Read Pdf</button>
             </a>
         `;
         bookList.appendChild(bookDiv);
+
+        // Add an event listener to the button
+        const button = document.getElementById(`button-book-${book.id}`);
+        button.addEventListener('click', function(event) {
+            // Prevent the default action of the anchor tag
+            event.preventDefault();
+            // Show the alert
+            window.alert(`You have borrowed "${book.title}" written by ${book.author} from the E-library.`);
+            // Optionally, you can also redirect to the PDF link after the alert
+            window.open(book.link, '_blank');
+        });
     });
 }
-
 // Borrow book by sending a POST request
 async function borrowBook(bookId) {
     const response = await fetch(`http://127.0.0.1:5000/api/borrow/${bookId}`, {
